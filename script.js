c6902ac97063d2d482139a9ade7ecd58aaf8ea63
U.S government <sabby.goodwin.2011@outlook.com>
2025-04-28 15:43:10 +0930

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ document.addEventListener('keydown', (e) => {
 
 // Stop walking or test1 animation when key is released
 document.addEventListener('keyup', (e) => {
-    if ((e.key === 'd' || e.key === 'D') || (e.key === 'a' || e.key === 'A')) {
+    if ((e.key === 'a' || e.key === 'A') || (e.key === 'd' || e.key === 'D')) {
         walking = false;
         clearInterval(animationInterval);  // Stop walking animation
         animationInterval = setInterval(animateIdle, 100);  // Resume idle animation
